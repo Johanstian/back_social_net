84c71ed0f194b703cdaacf6636a630b5648ac8b5
Johan Lopez <johan.lopez@bitechconsulting.co>
2024-11-04 15:18:46 -0500

    Upgrade size profile image

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,16 +1,3 @@
-// const multer = require('multer');
-
-// const storage = multer.diskStorage({
-//     filename: function(req, file, cb) {
-//         cb(null, file.originalname)
-//     }
-// });
-
-// const upload = multer({storage: storage});
-
-// module.exports = upload;
-
-
 const multer = require('multer');
 const path = require('path');
 
